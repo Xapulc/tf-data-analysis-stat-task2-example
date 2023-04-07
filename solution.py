e06eea4d258ca118755ac45a00fc82f40c37c60c
yuliazaitzeva <48553429+yuliazaitzeva@users.noreply.github.com>
2023-04-07 23:25:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,22 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1007460447 # Ваш chat ID, не меняйте название переменной
+
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    x_mean = x.mean()
+    x_var = np.var(x)
+    
+    # Calculate mean and variance of generated significance level
+    a = 0.032
+    b_mean = 2*x_mean - a
+    b_var = (b_mean - a)**2 / 12
+    
+    # Calculate confidence interval for upper bound b
+    loc = (a + b_mean) / 2
+    scale = np.sqrt(b_var) / np.sqrt(n)
+    return loc - scale * norm.ppf(1 - alpha / 2), loc - scale * norm.ppf(alpha / 2)
+
